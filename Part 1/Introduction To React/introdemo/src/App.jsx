81c1919b10b9c126c3b8d1cc22b44f4dad7a7709
Temi <limiajayi@gmail.com>
2025-06-28 12:30:16 +0100

    Exercise 1.5: Modified the parts and course variables into a single course object.

--- Part 1/Introduction To React/introdemo/src/App.jsx
+++ Part 1/Introduction To React/introdemo/src/App.jsx
@@ -36,53 +36,38 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  }
 
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  }
-
-  // const course = {
-  //   name: 'Half Stack application development',
-  //   parts: [
-  //     {
-  //       name: 'Fundamentals of React',
-  //       exercises: 10
-  //     },
-  //     {
-  //       name: 'Using props to pass data',
-  //       exercises: 7
-  //     },
-  //     {
-  //       name: 'State of a component',
-  //       exercises: 14
-  //     }
-  //   ]
-  // }
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
 
       <Content 
-      part1={part1.name} part2={part2.name} 
-      part3={part3.name} exercises1={part1.exercises} 
-      exercises2={part2.exercises} exercises3={part3.exercises}/>
+      part1={course.parts[0].name} part2={course.parts[1].name} 
+      part3={course.parts[2].name} exercises1={course.parts[0].exercises} 
+      exercises2={course.parts[1].exercises} exercises3={course.parts[2].exercises}/>
 
-      <Total exercises1={part1.exercises} 
-      exercises2={part2.exercises} 
-      exercises3={part3.exercises3} />
-      
+      <Total exercises1={course.parts[0].exercises} 
+      exercises2={course.parts[1].exercises} 
+      exercises3={course.parts[2].exercises} />
+
     </div>
   )
 }
