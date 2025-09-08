1a5f40062dd470fe15ea05358e69378f2cbd644a
Temi <limiajayi@gmail.com>
2025-09-08 15:48:27 +0100

    Ex 2.9: The Phonebook Step 4.

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -3,18 +3,15 @@ import Number from './components/Number'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchValue, setSearchValue] = useState('')
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -35,16 +32,26 @@ const App = () => {
     setPersons(persons.concat(newNameObject))
   }
 
+  //case insensitive search
+  const personsToShow = searchValue ? persons.filter(person => {return person.name.toLowerCase().includes(searchValue) || person.name.includes(searchValue)}) : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <p>
+        filter shown with 
+        <input type='text' value={searchValue} onChange={({ target }) => {setSearchValue(target.value)}} />
+      </p>
+
       <form onSubmit={addPerson}>
+
         <div>
           name: 
           <input 
           type='text'
           value={newName} 
-          onChange={handleNameChange} />
+          onChange={({ target }) => {setNewName(target.value)}} />
         </div>
 
         <div>
@@ -52,16 +59,18 @@ const App = () => {
           <input
           type='text'
           value={newNumber}
-          onChange={handleNumberChange} />
+          onChange={({ target }) => {setNewNumber(target.value)}} />
         </div>
+
         <div>
           <button type="submit">add</button>
         </div>
+
       </form>
       <h2>Numbers</h2>
 
       <div>
-        {persons.map((person) => {
+        {personsToShow.map((person) => {
           return (
             <Number 
             key={person.name} 
@@ -72,6 +81,7 @@ const App = () => {
 
     </div>
   )
+  
 }
 
 export default App
