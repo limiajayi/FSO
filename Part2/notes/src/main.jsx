539ef05885792d42ca9f51ce21d8c340cacdb7a1
Temi <limiajayi@gmail.com>
2025-09-18 16:49:39 +0100

    Workin with axios.

--- Part2/notes/src/main.jsx
+++ Part2/notes/src/main.jsx
@@ -1,5 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import axios from 'axios'
+
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+  })
 
 const notes = [
   {
