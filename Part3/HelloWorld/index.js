d550e4137b0706e0a652ab9a7f14ddce92572a4a
Temi <limiajayi@gmail.com>
2025-12-22 17:12:33 +0000

    FSO Part 3a.

--- Part3/HelloWorld/index.js
+++ Part3/HelloWorld/index.js
@@ -20,11 +20,30 @@ let notes = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello, World!</h1>')
+    response.send('<p>Welcome to the notes root API</p>')
 })
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id) //find is like filter but for one object
+    
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    notes = notes.filter(note => note.id !== id) //this is what the react state will likely see
+    //we would also use the backend to update the database!
+
+    response.status(204).end()
 })
 
 const PORT = 3001
